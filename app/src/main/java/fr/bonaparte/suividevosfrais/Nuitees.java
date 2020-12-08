1dcf3076851b18314f277e5128ac8c60d4cb7126
Alexis <alexisvedrenne482@gmail.com>
2020-12-08 15:04:13 +0100

    Ajout de la nouvelle activity et configuration de l'interface

---
+++ app/src/main/java/fr/bonaparte/suividevosfrais/Nuitees.java
@@ -0,0 +1,14 @@
+package fr.bonaparte.suividevosfrais;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Nuitees extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuitees);
+    }
+}
