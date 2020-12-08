81893f0f21c079da2fb74c2d5bdb658228cc8c4a
Alexis <alexisvedrenne482@gmail.com>
2020-12-08 15:51:54 +0100

    Finalisation de la deuxième activity Frais de nuitées

--- app/src/main/java/fr/bonaparte/suividevosfrais/Nuitees.java
+++ app/src/main/java/fr/bonaparte/suividevosfrais/Nuitees.java
@@ -2,13 +2,132 @@ package fr.bonaparte.suividevosfrais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.Locale;
 
 public class Nuitees extends AppCompatActivity {
+    private Integer annee ;
+    private Integer mois ;
+    private Integer qte ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuitees);
+        setTitle("GSB : Frais Nuitées");
+        // modification de l'affichage du DatePicker
+        Global.changeAfficheDate((DatePicker) findViewById(R.id.datNuitee), false) ;
+        // valorisation des propriétés
+        valoriseProprietes() ;
+        // chargement des méthodes événementielles
+        imgReturn_clic() ;
+        cmdValider_clic() ;
+        cmdPlus_clic() ;
+        cmdMoins_clic() ;
+        dat_clic() ;
+    }
+
+    /**
+     * Valorisation des propriétés avec les informations affichées
+     */
+    private void valoriseProprietes() {
+        annee = ((DatePicker)findViewById(R.id.datNuitee)).getYear() ;
+        mois = ((DatePicker)findViewById(R.id.datNuitee)).getMonth() + 1 ;
+        qte = 0 ;
+        Integer key = annee*100+mois ;
+        if (Global.listFraisMois.containsKey(key)) {
+            qte = Global.listFraisMois.get(key).getNuitee();
+        }
+        ((EditText)findViewById(R.id.txtnuitee)).setText(String.format(Locale.FRANCE, "%d", qte)) ;
+    }
+
+    /**
+     * Sur la selection de l'image : retour au menu principal
+     */
+    private void imgReturn_clic() {
+        findViewById(R.id.imgNuiteReturn).setOnClickListener(new ImageView.OnClickListener() {
+            public void onClick(View v) {
+                retourActivityPrincipale() ;
+            }
+        }) ;
+    }
+
+    /**
+     * Sur le clic du bouton valider : sérialisation
+     */
+    private void cmdValider_clic() {
+        findViewById(R.id.cmdNuiteeValider).setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Serializer.serialize(Global.listFraisMois,Nuitees.this) ;
+                retourActivityPrincipale() ;
+            }
+        }) ;
+    }
+
+    /**
+     * Sur le clic du bouton plus : ajout de 10 dans la quantité
+     */
+    private void cmdPlus_clic() {
+        findViewById(R.id.cmdNuitePlus).setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                qte+=1 ;
+                enregNewQte() ;
+            }
+        }) ;
+    }
+
+    /**
+     * Sur le clic du bouton moins : enlève 10 dans la quantité si c'est possible
+     */
+    private void cmdMoins_clic() {
+        findViewById(R.id.cmdNuiteMoins).setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                qte = Math.max(0, qte-1) ; // suppression de 10 si possible
+                enregNewQte() ;
+            }
+        }) ;
+    }
+
+    /**
+     * Sur le changement de date : mise à jour de l'affichage de la qte
+     */
+    private void dat_clic() {
+        final DatePicker uneDate = (DatePicker) findViewById(R.id.datNuitee);
+        uneDate.init(uneDate.getYear(), uneDate.getMonth(), uneDate.getDayOfMonth(), new DatePicker.OnDateChangedListener(){
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                valoriseProprietes() ;
+            }
+        });
+    }
+
+    /**
+     * Enregistrement dans la zone de texte et dans la liste de la nouvelle qte, à la date choisie
+     */
+    private void enregNewQte() {
+        // enregistrement dans la zone de texte
+        ((EditText)findViewById(R.id.txtnuitee)).setText(String.format(Locale.FRANCE, "%d", qte)) ;
+        // enregistrement dans la liste
+        Integer key = annee*100+mois ;
+        if (!Global.listFraisMois.containsKey(key)) {
+            // creation du mois et de l'annee s'ils n'existent pas déjà
+            Global.listFraisMois.put(key, new FraisMois(annee, mois)) ;
+        }
+        Global.listFraisMois.get(key).setNuitee(qte); ;
+    }
+
+    /**
+     * Retour à l'activité principale (le menu)
+     */
+    private void retourActivityPrincipale() {
+        Intent intent = new Intent(Nuitees.this, MainActivity.class) ;
+        startActivity(intent) ;
     }
 }
